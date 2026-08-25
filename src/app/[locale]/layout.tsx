d53a771b1d9d53ff6dde7b9b082111d53638b950
avrsrikanth <avrsrikanth@gmail.com>
2026-08-25 11:42:57 +0530

    fix: correct Google Search Console verification token

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -52,7 +52,7 @@ export async function generateMetadata({
       images: [image.url],
     },
     verification: {
-      google: "Q8idzl2UZNNAr50igNOC_nEXwQ5RknQUhkG-qHyGUFk",
+      google: "RQLNvbY_0Tis_G9jQPQgrkMGuZvQyQcXTzTB5kdgv4A",
     },
   };
 }
